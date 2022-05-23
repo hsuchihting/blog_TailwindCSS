ce1733da48affe018f2584ab7cf508b7f132fa76
timhsu <tim.hsu@tpisoftware.com>
2022-05-23 22:18:18 +0800

    blog first

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Blog 個人部落格實作開發
+
+## 使用技術
+
+1. Tailwind CSS 3.0
+2. jQuery 3.6.0
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mode: "jit",
+  content: ["./**/*.html", "./src/**/*.css", "./js/**/*.js"], //寫在這裡
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
