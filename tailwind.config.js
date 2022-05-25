a57dfcfa4f18720a3d5cd73067ab9ca14937a0a3
timhsu <tim.hsu@tpisoftware.com>
2022-05-26 00:05:19 +0800

    blog

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@ module.exports = {
   mode: "jit",
   content: ["./**/*.html", "./src/**/*.css", "./js/**/*.js"], //寫在這裡
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "#FFFFFF",
+        primary: "#406951",
+        text: "#666666",
+        gary: "#00000099",
+        highlight: "#FAF7EC",
+        peach: "#EFA2A2",
+        lightGray: "#999999",
+        secondary: "#CCCCCC",
+      },
+      fontFamily: {
+        "noto-sans": "Noto Sans TC",
+      },
+    },
   },
   plugins: [],
 };
