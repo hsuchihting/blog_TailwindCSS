1a67ef5fe9fe3c8cf3fb3c7a9d555ee04280ab66
timhsu <tim.hsu@tpisoftware.com>
2022-06-18 22:56:25 +0800

    rwd complete

--- js/all.js
+++ js/all.js
@@ -6,36 +6,76 @@ $(document).ready(function () {
   $(".menu-block").click(function () {
     $(".menu-close").show(); //*關閉 icon 顯示
     $(this).hide(); //*漢堡選單隱藏
-    $(".article-list").slideDown(500); //*列表向下滑動
+    $(".article-list").slideDown(700); //*列表向下滑動
+    //* 最新文章與最新回應初始狀態
+    $(".new-article-btn").find("button > svg").hide();
+    $(".new-article-btn").removeClass("bg-highlight");
+    $(".response-btn").find("button > svg").hide();
+    $(".response-btn").removeClass("bg-highlight");
   });
 
   //*關閉漢堡選單 icon
   $(".menu-close").click(function (e) {
     $(this).hide(); //*隱藏關閉 icon
-    $(".article-list").slideUp(500); //*文章列表向上滑動
+    $(".article-list").slideUp(700); //*文章列表向上滑動
     $(".menu-block").show(); //* 顯示漢堡選單
   });
 
-  //*文章列表按鈕
-  $(".article-btn").click(function (e) {
+  //*文章分類按鈕
+  $(".article-category-btn").click(function (e) {
     e.preventDefault();
-    $(this).toggleClass('active')
-    $(".article-category > ul").slideToggle(700);
-    // $(this).parent().parent().find(".article-category > ul").slideToggle(500);
-    // $(this)
-    //   .parent()
-    //   .parent()
-    //   .siblings()
-    //   .find(".article-category > ul")
-    //   .slideUp();
-    // $(this)
-    //   .parent()
-    //   .parent()
-    //   .siblings()
-    //   .find(".bg-highlight")
-    //   .removeClass("bg-highlight");
-    // $(this).parent().addClass("bg-highlight");
-    // $(this).parent().parent().siblings().find(".article-btn > svg").hide();
-    // $(this).find("svg").show();
+    //*文章分類按鈕顯示狀態
+    $(this).find("button > svg").show();
+    $(this).addClass("bg-highlight");
+    //*其他項目背景與按鈕不顯示
+    $(".article-category > ul").slideDown(700);
+    $(".new-articles > ul").slideUp(700);
+    $(".new-article-btn").find("button > svg").hide();
+    $(".new-article-btn").removeClass("bg-highlight");
+    $(".response > ul").slideUp(700);
+    $(".response-btn").find("button > svg").hide();
+    $(".response-btn").removeClass("bg-highlight");
+  });
+
+  //*最新文章按鈕
+  $(".new-article-btn").click(function (e) {
+    e.preventDefault();
+    //*最新文章按鈕顯示狀態
+    $(this).addClass("bg-highlight");
+    $(this).find("button > svg").show();
+    //*其他項目背景與按鈕不顯示
+    $(".new-articles > ul").slideDown(700);
+    $(".article-category > ul").slideUp(700);
+    $(".article-category-btn").find("button > svg").hide();
+    $(".article-category-btn").removeClass("bg-highlight");
+    $(".response > ul").slideUp(700);
+    $(".response-btn").find("button > svg").hide();
+    $(".response-btn").removeClass("bg-highlight");
+  });
+
+  //*最新回應按鈕
+  $(".response-btn").click(function (e) {
+    //*最新回應按鈕顯示狀態
+    e.preventDefault();
+    $(this).find("button > svg").show();
+    $(this).addClass("bg-highlight");
+    //*其他項目背景與按鈕不顯示
+    $(".response > ul").slideDown(700);
+    $(".new-articles > ul").slideUp(700);
+    $(".new-article-btn").find("button > svg").hide();
+    $(".new-article-btn").removeClass("bg-highlight");
+    $(".article-category > ul").slideUp(700);
+    $(".article-category-btn").removeClass("bg-highlight");
+  });
+
+  //* goTop
+  $(".go-top").click(function (e) {
+    e.preventDefault();
+    $("html,body").animate(
+      {
+        scrollTop: 0,
+      },
+      1000
+    );
   });
 });
